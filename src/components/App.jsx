f3da960e5c7eb13a579cfac1b5606721d22ab937
Nadezhda Kovalenko <nadina00@ukr.net>
2022-09-14 16:41:06 +0300

    вносит изменения в файлы

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ContactList } from 'components/ContactList/ContactList';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 import { useSelector, useDispatch  } from "react-redux";
 import {changeFilter}  from 'redux/action';
-import PropTypes from 'prop-types';
+import { Loader } from './Loader/Loader';
 import {useGetContactsQuery, useAddContactMutation} from '../redux/contactsSlice'
 
 export function App() {
  
   const { data, isLoading } = useGetContactsQuery()
-   console.log(isLoading)
-
-   const [addContact] = useAddContactMutation()
+  
+  const [addContact]= useAddContactMutation()
    
    const handleAddContact = async ({name, phone}) => {
     try{
-      await addContact({name, phone})
-     
-    }catch(error) {
+      await addContact({name, phone});
+      }catch(error) {
       console.log(error)
     }
    }
   
-
-
-  const dispatch = useDispatch();
+   const dispatch = useDispatch();
 
   const filterCont = useSelector(state => state.filter);
   console.log(filterCont)
@@ -48,7 +45,7 @@ export function App() {
       <h1>Phonebook</h1>
       <ContactForm onSubmit={handleAddContact} />
       <h2>Contacts</h2>
-  
+      {isLoading && <Loader/>}
       <ContactList contacts={getVisibleContacts} />
      
      <Filter value={filterCont} onChange={handleFindChange} />
